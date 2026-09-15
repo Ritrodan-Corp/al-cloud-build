9e6f9b44961ebc91bf8e7c32a4f11a3057a2ea30
dano-hash <ritrodano@gmail.com>
2026-09-15 10:40:04 -0700

    Narrow final raw-log credential false positive

--- tools/al-log-append.py
+++ tools/al-log-append.py
@@ -27,7 +27,9 @@ SENSITIVE_RE = re.compile(
     r"(?i)(authorization\s*:|bearer\s+[A-Za-z0-9._~+/=-]+|"
     r"(?:refresh|access)[_ -]?token\s*[:=]\s*\S+|"
     r"password\s*[:=]\s*\S+|cookie\s*:|"
-    r"client[_ -]?secret\s*[:=]\s*\S+|private[_ -]?key)"
+    r"client[_ -]?secret\s*[:=]\s*\S+|"
+    r"private[_ -]?key\s*[:=]\s*\S+|"
+    r"-----BEGIN (?:RSA |EC |OPENSSH )?PRIVATE KEY-----)"
 )
 
 
